73195d900f6885875e8cc3b945e9de064c0166c4
MadProgrammerGR <giorgozombo@yahoo.gr>
2018-05-20 22:28:03 +0300

    commit

--- program.py
+++ program.py
@@ -2,17 +2,31 @@ import networkx as nx
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('sx-stackoverflow.txt',delimiter=' ',dtype=int,header=None,names=['src','dest','time']).as_matrix()
-Tmax=data[:,2].max()
-Tmin=data[:,2].min()
+data = pd.read_csv('data/sx-stackoverflow-sorted.txt',nrows=100,delimiter=' ',dtype=int,header=None,names=['src','dest','time']).values
+Tmin = data[:,2].min()
+Tmax = data[:,2].max()
 
 N=10
 dt=(Tmax-Tmin)//N
+
+def dict_append(dict, key, value):
+    if key not in dict:
+        dict[key] = [value]
+    else:
+        dict[key] += [value]
+
+groups = {}
+for row in data:
+    index = (row[2]-Tmin)//dt
+    dict_append(groups,index,row)
+
+dict_append(groups,N-1,groups[N])
+del groups[N]
+print('Grouped:')
+for item in groups.items():
+    print(item)
+
 ##ToDo
-## save each graph in their on file,del data after that, load each graph seperated, save maybe some minor information like Tmin,Tmax
-G=[nx.Graph() for i in range(N)]
+## load each graph seperated
+#G=[nx.Graph() for i in range(N)]
 
-
-        
-        
-        
